62f2a5ec458b975e7f17e84935e604ba85a4026a
mochi-753 <naka.mochi11@gmail.com>
2025-10-04 21:29:05 +0900

    Initial commit

--- src/main/java/com/mochi_753/gaming_emi/mixin/SearchWidgetMixin.java
+++ src/main/java/com/mochi_753/gaming_emi/mixin/SearchWidgetMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(dev.emi.emi.screen.widget.EmiSearchWidget.class)
 public class SearchWidgetMixin {
-    @Redirect(method = "renderWidget", at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"))
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"))
     private boolean alwaysTrue(String instance, CharSequence sequence) {
         if ("jeb_".contentEquals(sequence)) {
             return true;
